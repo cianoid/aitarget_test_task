226a8a9f52e9856e568445213f3a9783cecd5b2c
Igor Shatava <cianoid@yandex.ru>
2021-11-29 21:12:12 +0300

    feat: add 'users' app
    Custom AUTH_USER_MODEL for this project

---
+++ mylibrary/users/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    last_name = models.CharField('Фамилия', max_length=150)
+    first_name = models.CharField('Имя', max_length=150)
+    middle_name = models.CharField('Отчество', max_length=150)
+    email = models.EmailField('Email')
