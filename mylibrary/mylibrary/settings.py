2ef993717f5e1443f3e96e1b11c01f9b069922c7
Igor Shatava <cianoid@yandex.ru>
2021-11-30 07:51:13 +0300

    fix: hide db credentials

--- mylibrary/mylibrary/settings.py
+++ mylibrary/mylibrary/settings.py
@@ -73,10 +73,10 @@ WSGI_APPLICATION = 'mylibrary.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'aitest',
-        'USER': 'aitestuser',
-        'PASSWORD': 'a1c843c3a',
-        'HOST': 'localhost',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
         'PORT': '',
     }}
 
