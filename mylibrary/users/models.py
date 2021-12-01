f03163394b88cfdfa6d16733160f9734a634df31
Igor Shatava <cianoid@yandex.ru>
2021-12-01 21:02:41 +0300

    feat: change middle_name requirements

--- mylibrary/users/models.py
+++ mylibrary/users/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class User(AbstractUser):
     last_name = models.CharField('Фамилия', max_length=150)
     first_name = models.CharField('Имя', max_length=150)
-    middle_name = models.CharField('Отчество', max_length=150)
+    middle_name = models.CharField('Отчество', max_length=150, blank=True)
     email = models.EmailField('Email')
