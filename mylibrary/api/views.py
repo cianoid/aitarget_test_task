8505b012e2f94c53d39c8a01a7172bc702d6ea18
Igor Shatava <cianoid@yandex.ru>
2021-12-02 23:13:51 +0300

    feat: add search by book name and author name

--- mylibrary/api/views.py
+++ mylibrary/api/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 
 from api.permissions import AdminWriteAccessPermission
 from api.serializers import (AuthorSerializer, BookSerializer,
@@ -20,6 +20,8 @@ class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     permission_classes = [AdminWriteAccessPermission,
                           permissions.IsAuthenticated]
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('name', 'author__last_name', 'author__first_name')
 
     def get_queryset(self):
         if self.request.user.is_staff:
