66cf33e4080e767b7778bd0837d4b93d57d5d94f
Igor Shatava <cianoid@yandex.ru>
2021-12-03 07:24:35 +0300

    feat: add email send when add new book

--- mylibrary/api/views.py
+++ mylibrary/api/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from rest_framework import viewsets, permissions, filters
+from core.email import send_email_using_bcc
 
 from api.permissions import AdminWriteAccessPermission
 from api.serializers import (AuthorSerializer, BookSerializer,
@@ -20,7 +21,7 @@ class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     permission_classes = [AdminWriteAccessPermission,
                           permissions.IsAuthenticated]
-    filter_backends = (filters.SearchFilter, )
+    filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'author__last_name', 'author__first_name')
 
     def get_queryset(self):
@@ -28,6 +29,34 @@ class BookViewSet(viewsets.ModelViewSet):
             return Book.objects.all()
 
         return Book.objects.filter(publication_year__lte=datetime.now().year)
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+        data = serializer.data
+        publication_year = data['publication_year']
+        current_year = datetime.now().year
+
+        if publication_year > current_year:
+            return None
+
+        author = Author.objects.get(pk=data['author'])
+        book_name = data['name']
+
+        followers = author.followers.all()
+
+        if not followers:
+            return None
+
+        send_email_using_bcc(
+            subject=f'Доступна книга "{book_name}" ({author})',
+            message='Привет!\n\n'
+                    'Только что на нашем сервисе появилась новая книга от '
+                    f'{author}!\n\n'
+                    f'{book_name}, {publication_year}г.\n\n'
+                    '---'
+                    f'\n© {current_year}, Сервис библиотеки ',
+            recipient_list=[follower.user.email for follower in followers])
 
 
 class FollowViewSet(viewsets.ModelViewSet):
